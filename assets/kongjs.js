66ce0b74e3af16a041718302a4df33cd2b67c582
chrisperez626 <chrisperez626@gmail.com>
2018-03-29 13:15:05 -0400

    base html

--- assets/kongjs.js
+++ assets/kongjs.js
@@ -2,14 +2,16 @@
 
 function ajax() {
 $.ajax({
-    url: "https://pixabay.com/api/?key=8524906-87de4c65edf77625335e21bd8&q=new+york&image_type=photo",
+    url: "https://pixabay.com/api/?key=8524906-87de4c65edf77625335e21bd8&q=new+orleans&image_type=photo",
     method: "GET"
 
 }).then(function(response){
     console.log(response)
     var newDiv = $("<img>")
-    newDiv.attr("src", response.hits[0].userImageURL)
-    $(".images").append(newDiv);
+    newDiv.attr("src", response.hits[1].webformatURL)
+    // newDiv.attr("width","200");
+    // newDiv.attr("height", "200");
+    $("#images").append(newDiv);
 })
 }
 
