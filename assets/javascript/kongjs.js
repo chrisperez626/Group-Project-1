9e56070d63b45e45f3e39dc409700d8065e95639
Seungha Kong <kseha1080@gmail.com>
2018-03-30 00:24:28 -0400

    event section backend finished

---
+++ assets/javascript/kongjs.js
@@ -0,0 +1,48 @@
+$(document.body).on("click", "#eventbtn", function(event){
+
+    event.preventDefault();
+
+    var cityZip = $("#zipinput").val()
+    
+    $.ajax({
+        url: "https://api.meetup.com/2/open_events.json?zip=" + cityZip + "&key=c5ae5417717fa335b7d8c774c32",
+        method: "GET"
+    }).then(function(response){
+        console.log(response)
+        
+        i = 3;
+
+        while (i < 8) {
+
+        var eventDiv = $("<div>");
+
+        var imageDiv = $("<img>");
+        imageDiv.attr("src", response.results[i].photo_url);
+        imageDiv.attr("alt", "Image not uploaded by group");
+
+        var titleDiv = $("<div>");
+        titleDiv.text("Title: " + response.results[i].group.name);
+
+        if (response.results[i].venue.zip === undefined) {
+
+            response.results[i].venue.zip = "";
+
+        }
+
+        var address = response.results[i].venue.name + " " + response.results[i].venue.address_1 + " " + response.results[i].venue.city + " " + response.results[i].venue.state + " " + response.results[i].venue.zip
+        var addressDiv = $("<div>");
+        addressDiv.text("Address: " + address);
+
+        var urlLink = $("<a>");
+        urlLink.attr("href", response.results[i].event_url);
+        urlLink.text("Learn more about this event");
+
+        $(eventDiv).append(imageDiv, titleDiv, addressDiv, urlLink);
+        $("#event-display").append(eventDiv);
+        
+        i++;
+
+        };
+        
+    });
+});
